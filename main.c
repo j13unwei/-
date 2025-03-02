6a5a72c0d80644c1c5f617ba3b0034c44e54b753
j13unwei <2663130550@qq.com>
2025-03-02 21:07:23 +0800

    The Second Commit

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 // 模拟获取温度值的函数
-float getTemperature() {
+float getTemperature(int t) {
     // 这里简单返回一个固定值，实际应用中应从传感器读取
-    return 25.0; 
+    return t; 
 }
 
 int main() {
-    float temperature = getTemperature();
+	int t;
+	scanf("%d",&t);
+    float temperature = getTemperature(t);
     printf("当前采集到的温度值为: %.2f 摄氏度\n", temperature);
     return 0;
 }
